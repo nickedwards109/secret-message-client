74d5509d88aa7b537fac9eb31d6f1c87ff1db92d
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-11 15:01:27 -0600

    Add type checking tests for Cipher methods
    Cipher#encrypt(text) should not throw a TypeError if a String is passed in. Cipher#decrypt(text) should not throw a TypeError if an encrypted String is passed in, but should throw a TypeError if an unencrypted String is passed in. Cipher methods should throw a TypeError if data is passed in that is of type Number, Object, Boolean, Null, or Undefined.

--- src/Cipher.test.js
+++ src/Cipher.test.js
@@ -4,6 +4,7 @@ describe('Cipher', () => {
 	it('encrypts and decrypts text', () => {
 		const cipher = new Cipher();
 		const data = 'a very secret string to encrypt';
+
 		const encrypted = cipher.encrypt(data);
 		expect(typeof encrypted).toEqual('string');
 		expect(encrypted).not.toEqual(data);
@@ -11,5 +12,36 @@ describe('Cipher', () => {
 		const decrypted = cipher.decrypt(encrypted);
 		expect(typeof decrypted).toEqual('string');
 		expect(decrypted).toEqual(data);
+	});
+
+	it('throws a TypeError if non-string data is passed into Cipher methods', () => {
+		const cipher = new Cipher();
+
+		const string_data = 'this is a string';
+		expect( () => { cipher.encrypt(string_data) }).not.toThrow(new TypeError);
+
+		const encrypted_data = cipher.encrypt(string_data);
+		expect( () => { cipher.decrypt(encrypted_data) }).not.toThrow(new TypeError);
+		expect( () => { cipher.decrypt(string_data)}).toThrow(new TypeError);
+
+	  const number_data = 1337;
+	  expect( () => { cipher.encrypt(number_data) }).toThrow(new TypeError);
+	  expect( () => { cipher.decrypt(number_data) }).toThrow(new TypeError);
+
+	  const object_data = { message: 'this is a message'};
+	  expect( () => { cipher.encrypt(object_data) }).toThrow(new TypeError);
+	  expect( () => { cipher.decrypt(object_data) }).toThrow(new TypeError);
+
+	  const boolean_data = true;
+	  expect( () => { cipher.encrypt(boolean_data) }).toThrow(new TypeError);
+	  expect( () => { cipher.decrypt(boolean_data) }).toThrow(new TypeError);
+
+	  const null_data = null;
+	  expect( () => { cipher.encrypt(null_data) }).toThrow(new TypeError);
+	  expect( () => { cipher.decrypt(null_data) }).toThrow(new TypeError);
+
+	  const undefined_data = undefined;
+	  expect( () => { cipher.encrypt(undefined_data) }).toThrow(new TypeError);
+	  expect( () => { cipher.decrypt(undefined_data) }).toThrow(new TypeError);
 	});
 });
