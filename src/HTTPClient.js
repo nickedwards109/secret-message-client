653b30b1689084abe7841a0c971494161d08dd9e
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-15 15:35:31 -0600

    Implement generating a digital signature
    This commit implements HTTPClient#generate_signature(httpMethod, url, httpVersion, key), which takes four strings as arguments and returns a string. The return value is a digital signature which is the SHA-256 HMAC of the HTTP request line.
    
    This commit removes HTTPClient#sign_request(xhr, httpMethod, url, httpVersion), which not only generated a signature but also added it as a header on an XHR object. That approach did multiple things in one method and was hard to test. The new approach, where the method for generating a signature only generates the signature and does nothing else, is more testable and is more in line with the single responsibility principle.

--- src/HTTPClient.js
+++ src/HTTPClient.js
@@ -1,48 +1,14 @@
-import Cipher from './Cipher';
+import Hmac from './HMAC';
 
 class HTTPClient {
-  constructor(cipher) {
-    this.cipher = cipher;
-  }
-
-  sign_request(xhr, method, url, httpVersion) {
-    const upcase_HTTP_method = method.toUpperCase();
+  generate_signature(httpMethod, url, httpVersion, key) {
+    const upcase_HTTP_method = httpMethod.toUpperCase();
     const request_line = upcase_HTTP_method + ' ' + url + ' ' + httpVersion;
-    const signature = this.cipher.encrypt(request_line);
-    const initialization_vector = String(this.cipher.initialization_vector);
-    xhr.open(method, url);
 
-    // This comment is duplicated in the unit test for this method.
-    // It turns out that an XMLHttpRequest object has no method for getting the
-    //   values of request headers once they have been set. See the docs, where
-    //   there are methods for setting request headers and getting response
-    //   headers, but no methods for getting request headers:
-    //   https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-    // I'd still like to test that the signature is being generated, but I can't
-    //   directly get the values of headers from the XHR object.
-    // So, I'm going to return an object from this method that contains both the
-    //   XHR object itself as well as the signature and initialization vector.
-    //   This will make it possible to unit test that the signature and
-    //   initialization vector are in scope. BUT, we are still
-    //   not testing that these values were set in request headers.
+    const hmac = new Hmac();
+    const signature = hmac.hash(request_line, key);
 
-    // This all means that the following line of code must not be deleted!
-    // If you delete these two lines of code, the tests will still pass, but you
-    //   will be sending unsigned requests.
-    xhr.setRequestHeader('Authorization', signature);
-    xhr.setRequestHeader('IV', initialization_vector);
-
-    // You can manually verify that the Authorization and IV headers are being
-    //   set by uncommenting the following line and visually observing that the
-    //   Authorization and IV headers exist and have strings as their values.
-    // console.log(xhr);
-
-    const results = { xhr: xhr, signature: signature, initialization_vector: initialization_vector };
-    return results;
-  }
-
-  send_request(xhr) {
-    xhr.send();
+    return signature;
   }
 }
 
