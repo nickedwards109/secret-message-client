97711c6ca8e337b64d8dc608ca9c12973c918fcd
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-15 15:28:14 -0600

    Add a test for generating a digital signature
    This commit adds a test which checks that HTTPClient#generate_signature(httpMethod, url, httpVersion, key) generates a digital signature which is the SHA-256 HMAC of the HTTP request line. It also makes a minor syntactical change to the test for HMAC#hash(data, key) by setting the expected hash as a variable that is compared to the actual hash.
    
    It also removes a test for some prior functionality which I will remove. The method HTTPClient#sign_request(xhr, httpMethod, url, httpVersion), which took an XHR object and HTTP parameters and added a digital signature to the XHR, will be removed. It will be replaced by HTTPClient#generate_signature(httpMethod, url, httpVersion, key), which will only generate a digital signature and not add it as a header to an XHR object.
    
    The intention is to make the digital signature more adherent to web conventions by using SHA-256 HMAC to generate it, and also to make the signature generation method more testable and more adherent to the single responsibility principle by only assigning it the job of generating a signature.

--- src/HMAC.test.js
+++ src/HMAC.test.js
@@ -6,7 +6,9 @@ describe('hmac', () => {
     const key = '56a608dd0976a0e4727e6be306dcfe20fbb8f9017ae34ad82ae338a701b53ddde73a7ab2daf08ef54d375160996c3fb4bf5eab21d73a8eeb9246f93195598cc7';
     const data = 'this is some string data';
     const hash = hmac.hash(data, key);
+    const expected_hash = '547167367d838790107de41e4c443837703192f92cbc98af64989ddbf5e88eca';
+
     expect(typeof hash).toEqual('string');
-    expect(hash).toEqual('547167367d838790107de41e4c443837703192f92cbc98af64989ddbf5e88eca');
+    expect(hash).toEqual(expected_hash);
   })
 });
